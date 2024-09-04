2404c6cbfd5df5610d3bbcd6262196a63a7240a6
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-09-04 19:57:00 +0300

    Task0[Add]: UTF-8 Validation Question

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""UTF-8 Validation"""
+
+
+def validUTF8(data: list[int]) -> bool:
+    """
+    Function that determines if a given data set represents a valid
+    UTF-8 encoding.
+    """
+    remaining_bytes = 0
+
+    for byte in data:
+        if remaining_bytes == 0:
+            if (byte >> 5) == 0b110:
+                remaining_bytes = 1
+            elif (byte >> 4) == 0b1110:
+                remaining_bytes = 2
+            elif (byte >> 3) == 0b11110:
+                remaining_bytes = 3
+            elif (byte == 7) != 0:
+                return False
+        else:
+            if (byte >> 6) != 0b10:
+                return False
+            remaining_bytes -= 1
+    return remaining_bytes == 0
