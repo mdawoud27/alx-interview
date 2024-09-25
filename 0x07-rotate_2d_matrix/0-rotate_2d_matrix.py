19b1123ba7089f2806d38c18f1b5c08370ddb6b5
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-09-25 10:10:29 +0300

    Task0[Add]: Rotate 2D Matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Rotate 2D Matrix"""
+
+def rotate_2d_matrix(matrix):
+  """Rotate 2D Matrix"""
+  for i in range(len(matrix)):
+    for j in range(i + 1, len(matrix)):
+      matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+  
+  for row in matrix:
+    # print(row)
+    row.reverse()
