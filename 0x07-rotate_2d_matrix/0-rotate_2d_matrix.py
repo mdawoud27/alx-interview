ce3bc0a34b3d0da135c2cdd0bb98704ce2cc8eec
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-09-25 10:15:34 +0300

    fix the code format

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Rotate 2D Matrix"""
 
+
 def rotate_2d_matrix(matrix):
-  """Rotate 2D Matrix"""
-  for i in range(len(matrix)):
-    for j in range(i + 1, len(matrix)):
-      matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-  
-  for row in matrix:
-    # print(row)
-    row.reverse()
+    """Rotate 2D Matrix"""
+    for i in range(len(matrix)):
+        for j in range(i + 1, len(matrix)):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for row in matrix:
+        # print(row)
+        row.reverse()
